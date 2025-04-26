f329b81c3384d3295cc77774f9d4ce6066ea7436
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2025-04-26 10:08:26 +0800

    Create 16x16 grid of divs

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const grid = document.querySelector("#grid");
 
-function column(size) {
+let size;
+size = 16;
+
+for (let i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    row.setAttribute("class", "row");
+    grid.appendChild(row);
+
     for (let i = 0; i < size; i++) {
         const div = document.createElement("div");
-        div.style.cssText = `flex-basis: calc(100%/${size}); aspect-ratio: 1/1`
-        grid.appendChild(div);
+        row.appendChild(div);
     }
 }
-
-function row(size) {
-    for (let i = 0; i < size; i++) {
-        column(size);
-    }
-}
-
-row(16);
-
