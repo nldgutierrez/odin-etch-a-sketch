12fd5a8b5440105009b99d54269f2314d0f2d9f2
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2025-04-26 11:16:18 +0800

    Add a button to create new grid
    Create createGrid function to get grid size from
    user input and generate a new grid, triggered by
    button click.

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const grid = document.querySelector("#grid");
+const input = document.querySelector(".user-input")
 
 let size;
-size = 16;
 
-for (let i = 0; i < size; i++) {
+const button = document.createElement("button");
+button.setAttribute("class", "button");
+button.textContent = 'Set grid size';
+input.appendChild(button);
+
+button.addEventListener("click", createGrid);
+
+function createGrid() {
+    grid.innerHTML = '';
+
+    while (true) {
+        size = prompt('Enter grid size:');
+        size = Number(size);
+        if (size < 101 && size > 0) {
+            break;
+        } else {
+            alert('Enter a number from 1-100');
+            continue;
+        }
+    }
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        grid.appendChild(row);
+    
+        for (let i = 0; i < size; i++) {
+            const div = document.createElement("div");
+            row.appendChild(div);
+    
+            div.addEventListener("mouseenter", ()=> {
+                div.setAttribute("class", "sketch");
+            });
+        }
+    }
+}
+
+for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
     grid.appendChild(row);
 
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < 16; i++) {
         const div = document.createElement("div");
         row.appendChild(div);
 
